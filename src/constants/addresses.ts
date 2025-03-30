7f515ae86c777c65e35a321f8af953e5e72a8a8a
supersorbet <artificialperiod@gmail.com>
2025-03-29 22:17:10 -0400

    multicall

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -51,12 +51,12 @@ export const MULTICALL_ADDRESS: AddressMap = {
     SupportedChainId.OPTIMISM,
     SupportedChainId.POLYGON_MUMBAI,
     SupportedChainId.POLYGON,
-    SupportedChainId.BASED,
   ]),
   [SupportedChainId.ARBITRUM_ONE]: '0xadF885960B47eA2CD9B55E6DAc6B42b7Cb2806dB',
   [SupportedChainId.ARBITRUM_RINKEBY]: '0xa501c031958F579dB7676fF1CE78AD305794d579',
   [SupportedChainId.CELO]: CELO_MULTICALL_ADDRESS,
   [SupportedChainId.CELO_ALFAJORES]: CELO_MULTICALL_ADDRESS,
+  [SupportedChainId.BASED]: '0x0000000000000000000000000000000000000000',
 }
 
 export const SWAP_ROUTER_ADDRESSES: AddressMap = {
