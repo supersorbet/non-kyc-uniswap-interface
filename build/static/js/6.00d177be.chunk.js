77403f32a17e9f2e2de70fb641aa4de70608b74f
supersorbet <artificialperiod@gmail.com>
2025-03-31 10:29:32 -0400

    Fix GraphQL schema issues and simplify build process

---
+++ build/static/js/6.00d177be.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[6],{1717:function(n,e,r){"use strict";r.d(e,"b",(function(){return t})),r.d(e,"a",(function(){return i}));r(326),r(1654);var t="_2kdvfw1",i="_2kdvfw3 _2kdvfw1 rgw6ez69u rgw6ez68m rgw6ez4am"}}]);
